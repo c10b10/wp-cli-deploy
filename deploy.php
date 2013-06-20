2539139dcb75693f58e8cbb683301e6594096a58
Alex Ciobica <alex.ciobica@gmail.com>
2013-06-20 19:06:30 +0300

    Update constant management.
    * Updated private helper method names.
    * Management of constants is now made from a static class property
        * Required, Optional, and Copy from policies are supported
    * Updated %ENV%_PATH to %ENV%_SSH_PATH for consistency.
    * %ENV%_SSH_DB_HOST, %ENV%_SSH_DB_USER and %ENV%_SSH_DB_PATH are optional as
    they are duplicated from %ENV%_SSH_HOST, %ENV%_SSH_USER, and %ENV%_SSH_PATH
    respectively.

--- deploy.php
+++ deploy.php
@@ -9,14 +9,40 @@
 class WP_Deploy_Flow_Command extends WP_CLI_Command {
 
 	protected static $_env;
+
+	/**
+	 * List of unprefixed constants that need to be defined in wp-config.php.
+	 * About the value:
+	 * * true: constant is required.
+	 * * false: constant is optional.
+	 * * 'constant_name': if missing, copy value from that constant
+	 * TODO: Add subcommand dependency
+	 */
+	static $constants = array(
+		'url' => true,
+		'ssh_host' => true,
+		'ssh_user' => true,
+		'ssh_path' => true,
+		'ssh_db_host' => 'ssh_host',
+		'ssh_db_user' => 'ssh_user',
+		'ssh_db_path' => 'ssh_path',
+		'db_host' => true,
+		'db_user' => true,
+		'db_port' => false,
+		'db_name' => true,
+		'db_password' => true,
+		'locked' => false,
+		'remove_admin' => false,
+	);
+
 	/**
 	 * Push local to remote
 	 *
-	 * @synopsis <environment> --what=<db,uploads>
+	 * @synopsis <environment> --what=<what>
 	 */
 	public function push( $args, $assoc_args ) {
 
-		$settings = self::_prepare_and_extract( $args );
+		$settings = self::_get_sanitized_args( $args );
 
 		if ( $settings['locked'] === true ) {
 			WP_CLI::error( "$env environment is locked, you cannot push to it." );
@@ -34,8 +60,9 @@ class WP_Deploy_Flow_Command extends WP_CLI_Command {
 				WP_CLI::line( "Don't know how to deploy: $item" );
 		}
 
+		/** TODO Remove this. */
 		/* if ( $remove_admin === true ) { */
-		/* 	$com = "ssh $ssh_user@$ssh_host \"cd $path;rm -Rf wp-login.php\""; */
+		/* 	$com = "ssh $ssh_user@$ssh_host \"cd $ssh_path;rm -Rf wp-login.php\""; */
 		/* 	WP_CLI::line( $com ); */
 		/* 	WP_CLI::launch( $com ); */
 		/* } */
@@ -66,7 +93,7 @@ class WP_Deploy_Flow_Command extends WP_CLI_Command {
 		$commands = array(
 			array( "wp db export $backup_name.sql", true ),
 			array( "wp search-replace $siteurl $url", true ),
-			array( 'wp search-replace ' . untrailingslashit( ABSPATH ) . ' ' . untrailingslashit( $path ), true ),
+			array( 'wp search-replace ' . untrailingslashit( ABSPATH ) . ' ' . untrailingslashit( $ssh_path ), true ),
 			array( "wp db dump $dump_name.sql", true ),
 
 			array( "wp db import $backup_name.sql", true ),
@@ -96,7 +123,7 @@ class WP_Deploy_Flow_Command extends WP_CLI_Command {
 	private function _rsync_files( $source_path, $settings ) {
 
 		extract( $settings );
-		$remote_path = $path . '/';
+		$remote_path = $ssh_path . '/';
 		/** TODO Manage by flag. */
 		$exclude = array(
 			'.git',
@@ -129,8 +156,10 @@ class WP_Deploy_Flow_Command extends WP_CLI_Command {
 		WP_CLI::launch( $command );
 	}
 
-	public function pull( $args = array() ) {
-		extract( self::_prepare_and_extract( $args ) );
+	/** TODO */
+	private function pull( $args = array() ) {
+
+		extract( self::_get_sanitized_args( $args ) );
 
 		$const = strtoupper( ENVIRONMENT ) . '_LOCKED';
 		if ( constant( $const ) === true ) {
@@ -141,17 +170,18 @@ class WP_Deploy_Flow_Command extends WP_CLI_Command {
 		$wpdb = new wpdb( $db_user, $db_password, $db_name, $host );
 		$path = ABSPATH;
 		$url = get_bloginfo( 'url' );
-		$dist_path  = constant( self::config_constant( 'path' ) ) . '/';
+		$dist_path  = constant( self::_prefix_constant( 'path' ) ) . '/';
 		$command = "ssh $ssh_user@$ssh_host  \"cd $dist_path;wp migrate to $path $url dump.sql\" && scp $ssh_user@$ssh_host:$dist_path/dump.sql .";
 		WP_CLI::launch( $command );
 		WP_CLI::launch( 'wp db import dump.sql' );
 		self::pull_files( $args );
 	}
 
-	public function pull_files( $args = array() ) {
+	/** TODO */
+	private function pull_files( $args = array() ) {
 
 		WP_CLI::line( 'pulling files' );
-		extract( self::_prepare_and_extract( $args ) );
+		extract( self::_get_sanitized_args( $args ) );
 
 		$const = strtoupper( ENVIRONMENT ) . '_LOCKED';
 		if ( constant( $const ) === true ) {
@@ -161,7 +191,7 @@ class WP_Deploy_Flow_Command extends WP_CLI_Command {
 
 		if ( $ssh_host ) {
 			$dir = wp_upload_dir();
-			$dist_path  = constant( self::config_constant( 'path' ) ) . '/';
+			$dist_path  = constant( self::_prefix_constant( 'path' ) ) . '/';
 			$remote_path = $dist_path;
 			$local_path = ABSPATH;
 
@@ -173,9 +203,10 @@ class WP_Deploy_Flow_Command extends WP_CLI_Command {
 
 	}
 
-	public static function _prepare_and_extract( $args ) {
-		$out = array();
+	private static function _get_sanitized_args( $args ) {
+
 		self::$_env = $args[0];
+
 		$errors = self::_validate_config();
 		if ( $errors !== true ) {
 			foreach ( $errors as $error ) {
@@ -183,7 +214,8 @@ class WP_Deploy_Flow_Command extends WP_CLI_Command {
 			}
 			return false;
 		}
-		$out = self::config_constants_to_array();
+
+		$out = self::_get_constants();
 		$out['env'] = self::$_env;
 		$out['db_user'] = escapeshellarg( $out['db_user'] );
 		$out['db_host'] = escapeshellarg( $out['db_host'] );
@@ -193,9 +225,16 @@ class WP_Deploy_Flow_Command extends WP_CLI_Command {
 	}
 
 	protected static function _validate_config() {
+
+		/** Required constants have their value set to true. */
+		$required = array_keys( array_filter(
+			self::$constants,
+			function($v) { return $v === true; }
+		) );
+
 		$errors = array();
-		foreach ( array( 'path', 'url', 'db_host', 'db_user', 'db_name', 'db_password' ) as $postfix ) {
-			$required_constant = self::config_constant( $postfix );
+		foreach ( $required as $const ) {
+			$required_constant = self::_prefix_constant( $const );
 			if ( ! defined( $required_constant ) ) {
 				$errors[] = "$required_constant is not defined";
 			}
@@ -204,17 +243,23 @@ class WP_Deploy_Flow_Command extends WP_CLI_Command {
 		return $errors;
 	}
 
-	protected static function config_constant( $postfix ) {
+	protected static function _get_constants() {
 
-		return strtoupper( self::$_env.'_'.$postfix );
-	}
-
-	protected static function config_constants_to_array() {
 		$out = array();
-		foreach ( array( 'locked', 'path', 'ssh_db_path', 'url', 'db_host', 'db_user', 'db_port', 'db_name', 'db_password', 'ssh_db_host', 'ssh_db_user', 'ssh_db_path', 'ssh_host', 'ssh_user', 'remove_admin' ) as $postfix ) {
-			$out[$postfix] = defined( self::config_constant( $postfix ) ) ? constant( self::config_constant( $postfix ) ) : null;
+
+		foreach ( self::$constants as $const => $requirement ) {
+			if ( defined( self::_prefix_constant( $const ) ) ) {
+				$out[$const] = constant( self::_prefix_constant( $const ) );
+			} elseif ( is_string( $requirement ) ) {
+				$out[$const] = constant( self::_prefix_constant( $requirement ) );
+			}
 		}
 		return $out;
+	}
+
+	protected static function _prefix_constant( $name ) {
+
+		return strtoupper( self::$_env . '_' . $name );
 	}
 
 	private static function _trim_url( $url ) {
@@ -227,9 +272,8 @@ class WP_Deploy_Flow_Command extends WP_CLI_Command {
 			$url = 'http://' . $url;
 		}
 
-		$url_parts = parse_url( $url );
-
 		/** Remove www. */
+		$url_parts = parse_url( $url );
 		$domain = preg_replace( '/^www\./', '', $url_parts['host'] );
 
 		return $domain;
